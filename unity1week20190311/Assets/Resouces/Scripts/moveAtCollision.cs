65ac66d58d155be6d3666069dc253858b622854c
tamiwo <tamiwo.otutama@gmail.com>
2019-03-13 01:50:47 +0900

    update:背景の無限スクロール

---
+++ unity1week20190311/Assets/Resouces/Scripts/moveAtCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveAtCollision : MonoBehaviour
+{
+
+    public Vector2 move;
+    public string colliderName; 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == colliderName)
+        {
+            Debug.Log("collision:" + colliderName);
+            var pos = transform.position;
+            Vector2 newPos = new Vector2(pos.x + move.x, pos.y + move.y);
+            transform.position = newPos;
+        }
+    }
+}
