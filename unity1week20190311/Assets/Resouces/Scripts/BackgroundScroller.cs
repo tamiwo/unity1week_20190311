3fe26cc0466d9b4119606a8e2d7d3db779325b96
tamiwo <tamiwo.otutama@gmail.com>
2019-03-13 01:18:09 +0900

    update:背景のスクロール

---
+++ unity1week20190311/Assets/Resouces/Scripts/BackgroundScroller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    public GameObject backgroundPrefab;
+    public Vector2 backgroundSize;
+    public Vector2 speed;
+    private GameObject[] backgrounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgrounds = new GameObject[3];
+        //3つ並べる
+        backgrounds[0] = Instantiate(backgroundPrefab,transform);
+        backgrounds[1] = Instantiate(backgroundPrefab,transform);
+        backgrounds[2] = Instantiate(backgroundPrefab,transform);
+        Vector2 pos = transform.position;
+        backgrounds[0].transform.position =  backgroundSize*2;
+        backgrounds[2].transform.position =  -backgroundSize*2;
+       
+        Debug.Log(backgrounds[0].transform.position);
+
+        SetVelocity(speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SetVelocity(Vector2 speed)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            var r = backgrounds[i].GetComponent<Rigidbody2D>();
+            r.velocity = speed;
+        }
+    }
+}
