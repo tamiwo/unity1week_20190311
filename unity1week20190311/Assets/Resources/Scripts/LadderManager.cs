735cd321dc866d026d155eb203d1007dd00f2715
tamiwo <tamiwo.otutama@gmail.com>
2019-03-14 02:07:12 +0900

    update:ドラッグで線を引く

--- unity1week20190311/Assets/Resources/Scripts/LadderManager.cs
+++ unity1week20190311/Assets/Resources/Scripts/LadderManager.cs
@@ -4,20 +4,57 @@ using UnityEngine;
 
 public class LadderManager : MonoBehaviour
 {
+    public GameObject LadderSprite;
+
+    bool isDragging = false;
+    Vector3 startPos;
+    LineRenderer line;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //line = Instantiate(LadderSprite);
+        line = gameObject.GetComponent<LineRenderer>();
+        // 線の幅
+        line.startWidth = 50f;
+        line.endWidth = 50f;
+        // 頂点の数
+        line.positionCount = 2;
+        line.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 左ボタンクリック
+        // ドラッグ開始
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            startPos.z = 0f;
+            Debug.Log("LeftClickDown:" + startPos);
+            line.enabled = true;
+            line.SetPosition(0, startPos);
+            line.SetPosition(1, startPos);
+            isDragging = true;
+            
+        }
+
+        // ドラッグ中
+        if (isDragging)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0f;
+            line.SetPosition(1, mousePos);
+        }
+
+        // ドラッグ終わり
         if (Input.GetMouseButtonUp(0))
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log("LeftClick:" + mousePos);
+            Debug.Log("LeftClickUp:" + mousePos);
+            isDragging = false;
+            //line.enabled = false;
         }
     }
 }
