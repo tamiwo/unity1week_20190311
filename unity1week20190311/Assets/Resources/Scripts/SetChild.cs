cd78abcb6fc392fd2c39b46cfb2790fe06ba7db8
tamiwo <tamiwo.otutama@gmail.com>
2019-03-15 02:12:44 +0900

    update:item生成時に背景に追加するためのスクリプト追加

---
+++ unity1week20190311/Assets/Resources/Scripts/SetChild.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetChild : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("collision tag: " + collision.tag );
+
+        if (collision.tag == "Scroll")
+        {
+            var tf = collision.gameObject.transform;
+            tf.SetParent(this.transform);
+        }
+    }
+}
