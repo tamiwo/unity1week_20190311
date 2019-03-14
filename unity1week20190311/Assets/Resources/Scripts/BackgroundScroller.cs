98ab2f55bc0e24058bb4dca5a2c5426b8fdedfe6
tamiwo <tamiwo.otutama@gmail.com>
2019-03-15 00:45:57 +0900

    fix:backgroundはあらかじめ配置しておくように

--- unity1week20190311/Assets/Resources/Scripts/BackgroundScroller.cs
+++ unity1week20190311/Assets/Resources/Scripts/BackgroundScroller.cs
@@ -4,23 +4,13 @@ using UnityEngine;
 
 public class BackgroundScroller : MonoBehaviour
 {
-    public GameObject backgroundPrefab;
-    public Vector2 backgroundSize;
+
     public Vector2 speed;
-    private GameObject[] backgrounds;
+    public GameObject[] backgrounds;
 
     // Start is called before the first frame update
     void Start()
     {
-        backgrounds = new GameObject[3];
-        //3つ並べる
-        backgrounds[0] = Instantiate(backgroundPrefab,transform);
-        backgrounds[1] = Instantiate(backgroundPrefab,transform);
-        backgrounds[2] = Instantiate(backgroundPrefab,transform);
-        Vector2 pos = transform.position;
-        backgrounds[0].transform.position =  backgroundSize*2;
-        backgrounds[2].transform.position =  -backgroundSize*2;
-       
         Debug.Log(backgrounds[0].transform.position);
 
         SetVelocity(speed);
@@ -31,13 +21,12 @@ public class BackgroundScroller : MonoBehaviour
     {
 
     }
-
-    void SetVelocity(Vector2 speed)
+    void SetVelocity(Vector2 velocity)
     {
         for (int i = 0; i < 3; i++)
         {
             var r = backgrounds[i].GetComponent<Rigidbody2D>();
-            r.velocity = speed;
+            r.velocity = velocity;
         }
     }
 }
