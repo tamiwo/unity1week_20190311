307fa0ec0df186ddf754097db582210d2bf5ec6e
tamiwo <tamiwo.otutama@gmail.com>
2019-03-17 13:18:44 +0900

    GoldとEnemyが重なった時にGoldは消す
    効果がないようだ...

---
+++ unity1week20190311/Assets/Resources/Scripts/ManagerGoldCollider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerGoldCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // enemyと同じ位置に生成されて場合は消す
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(this.gameObject);
+            Debug.Log("Enamy exsit");
+        }
+    }
+}
