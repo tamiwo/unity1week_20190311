1e11743168fe2d350b8231159ad906609608bb19
tamiwo <tamiwo.otutama@gmail.com>
2019-03-17 20:09:40 +0900

    fix:GameOverScene

--- unity1week20190311/Assets/ReturnButton.cs
+++ unity1week20190311/Assets/ReturnButton.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class ReturnButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadGameScene()
     {
-        
+        FadeManager.FadeOut(1);
+        //SceneManager.LoadScene("GameScene");
     }
 }
