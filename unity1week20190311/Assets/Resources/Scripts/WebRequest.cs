4ec6ed7da6e7c8150c96839cfaf1c98179567e5d
tamiwo <tamiwo.otutama@gmail.com>
2019-03-23 10:42:47 +0900

    update:HPに飛ぶスクリプト追加

---
+++ unity1week20190311/Assets/Resources/Scripts/WebRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebRequest : MonoBehaviour
+{
+
+    public void Open(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
